646dd0a97cf6cb0e163f45669952d175c2674a11
topba <66887206+topba@users.noreply.github.com>
2021-06-20 23:19:12 +0300

    Java ile iki sayının EBOB ve EKOK değerlerini "While Döngüsü" kullanarak yazınız.

--- src/EbobEkok.java
+++ src/EbobEkok.java
@@ -2,10 +2,17 @@ public class EbobEkok {
     public static void main(String[] args) {
         int first=6;
         int second=8;
-
+int ebob=0;
         int i=1;
-        while(i<8){
+        while(i<6){
+            if(first%i==0&&second%i==0){
+               ebob=i;
+            }
+            i++;
 
         }
+        System.out.println("Ebob: "+ ebob);
+        int ekok =(first*second)/ebob;
+        System.out.println("EKOK"+ekok);
     }
 }
