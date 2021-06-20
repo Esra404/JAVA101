982bf39196a66ff86582075c48adf4a283a1b465
topba <66887206+topba@users.noreply.github.com>
2021-06-21 00:01:22 +0300

    asal sayi 1-100

--- src/asalSay/AsalSayi.java
+++ src/asalSay/AsalSayi.java
@@ -2,10 +2,17 @@ package asalSay;
 
 public class AsalSayi {
     public static void main(String[] args) {
+        for(int i=2; i<=100; i++){
+            int check=0;
+            for(int j=2; j<i; j++){
+                if(i%j==0){
+                check=1;
+                break;}
+            }
+            if(check==0){
+                System.out.println(" "+i);
+            }
+        }
 
-    }
-    public  static boolean asalMethod(int a){
-
-  return  true;
     }
 }
