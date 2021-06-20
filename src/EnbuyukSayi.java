6a5a8fcd3597a3afda097eb6a317609eccf6e3c5
topba <66887206+topba@users.noreply.github.com>
2021-06-20 23:40:10 +0300

    Girilen Sayılardan Min ve Max Değerli Bulan Program

---
+++ src/EnbuyukSayi.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EnbuyukSayi {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("enter the number of numbers you wnt to test ");
+        int n= in.nextInt();
+
+
+       int big=0;
+       int small=0;
+
+        for(int i=1; i<=n; i++){
+            System.out.println("enter a number ");
+            int m=in.nextInt();
+            if(i==1){
+                big=m;
+                small=m;
+            }
+            if(m>big){
+                big=m;
+            }
+            if(m<small){
+                small=m;
+            }
+
+        }
+        System.out.println("big: "+ big+ " small: "+small);
+    }
+}
