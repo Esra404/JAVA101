9ac60dc5eff0c4da7feed961e12ffc21dc80eeb7
topba <66887206+topba@users.noreply.github.com>
2021-06-20 23:54:39 +0300

    ucgenTersi

---
+++ src/TersUcgen.java
@@ -0,0 +1,13 @@
+public class TersUcgen {
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int m=2*10-1; m>=(2*i+1); m--){
+                System.out.print("^");
+            }
+            System.out.println();
+        }
+    }
+}
