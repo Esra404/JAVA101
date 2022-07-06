5b535c840749ecc47496b37e2c218ece5a26c220
USMANKHALID-GH <loftyusman@gmail.com>
2022-07-06 10:16:58 +0300

    StringBuilderAndStringBuffer

---
+++ src/StringBuilderAndStringBuffer.java
@@ -0,0 +1,11 @@
+public class StringBuilderAndStringBuffer {
+    public static void main(String[] args) {
+        StringBuilder builder;
+
+        String []strings={"usman","khalid"};
+        for(String s: strings){
+            builder= new StringBuilder(s);
+            System.out.println(builder.reverse());
+        }
+    }
+}
