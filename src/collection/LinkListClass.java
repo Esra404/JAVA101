6fcfde7d3f0f06139c355b9348be64793257877d
merve824 <loftyusman@gmail.com>
2023-02-18 13:52:07 +0300

    linklist queue

---
+++ src/collection/LinkListClass.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkListClass {
+    public static void main(String[] args) {
+        Queue<String> queue= new LinkedList<>();
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+        System.out.println(queue);
+    }
+}
