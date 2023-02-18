20cff2465672cf40f04ba9a0e0d971b367c1f9bb
merve824 <loftyusman@gmail.com>
2023-02-18 14:04:36 +0300

    linklist queue

--- src/collection/LinkListClass.java
+++ src/collection/LinkListClass.java
@@ -10,5 +10,9 @@ public class LinkListClass {
         queue.offer("B");
         queue.offer("C");
         System.out.println(queue);
+        System.out.println(queue.poll());
+
+        System.out.println(queue.peek());
+        System.out.println(queue);
     }
 }
